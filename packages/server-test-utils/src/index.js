3d14f56faa4645929160ea7203a218c210f14d0c
Quinn Langille <quinn@quinnlangille.com>
2019-01-14 14:20:48 -0500

    feat(ACQ-1331): add db flush in

--- packages/server-test-utils/src/index.js
+++ packages/server-test-utils/src/index.js
@@ -2,6 +2,8 @@ import renderToString from './renderToString'
 import render from './render'
 import config from './config'
 
+console.log('wheeatever')
+
 export default {
   renderToString,
   config,
